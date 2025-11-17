2aa64d12a9a74ad6fe8b50bd64b4fcbbb399a342
Станислав Шубин <sti.99@mail.ru>
2025-11-17 19:35:13 +0700

    new architecture

--- neuro/neuro.cpp
+++ neuro/neuro.cpp
@@ -1,145 +1,93 @@
 ﻿#include <iostream>
 #include <vector>
+#include <assert.h>
 #include <math.h>
 
 typedef float real;
 
-class neuron
+class network
 {
-private:
-	friend class layer;
-
-	std::vector<real> weights;
-	std::vector<neuron>* prev;
-	real bias=0;
-	real res=0;
-
 	static real f(real x)
 	{return 1./(1.+exp(-x));}
+	static real df(real x)
+	{return x*(1-x);}
 
-public:
+	struct neuron{real res;real bias;real error;};
 	
-	size_t size()const
-	{return weights.size();}
+	typedef std::vector<neuron> layer;
+	std::vector<layer> layers;
 
-	void calc()
-	{
-		res=0;
-		for(size_t i=0;i<size();i++)
-			res+=(*prev)[i].value()*weights[i];
-		res=f(res+bias);
-	}
+	typedef std::vector<std::vector<real>> matrix;
+	std::vector<matrix> weights;
 
-	real weight(size_t id)const
-	{return weights[id];}
-
-	void weight(size_t id,real v)
-	{weights[id]=v;}
-
-	real value()const
-	{return res;}
-
-	void value(real v)
-	{res=v;}
-};
-
-class layer
-{
-private:
-	std::vector<neuron> self;
 public:
-	layer(size_t size)
+
+	network(const std::vector<size_t> form)
 	:
-		self(size)
-	{}
-
-	void calc()
+		layers(form.size()),//количевство слоев
+		weights(form.size()-1)//количевство весов
 	{
-		for(neuron& e:self)
-			e.calc();
-	}
+		for(size_t i=0;i<form.size();i++)//каждый слой
+			layers[i].resize(form[i]);//заполняется нейронами
 
-	void connect(layer* prev)
-	{
-		for(neuron& e:self)
+		for(size_t j=0;j<form.size()-1;j++)//каждый вес
 		{
-			e.weights.resize(prev->size());
-			e.prev=&prev->self;
+			weights[j].resize(form[j]);//заполняется спискаи весов
+
+			for(size_t i=0;i<form[j];i++)//каждый список весов
+				weights[j][i].resize(form[j+1]);//заполняется весами
 		}
 	}
 
-	size_t size()const
-	{return self.size();}
-
-	real weight(size_t i,size_t j)const
-	{return self[i].weight(j);}
-
-	void weight(size_t i,size_t j,real v)
-	{self[i].weight(j,v);}
-
-	real value(size_t id)const
-	{return self[id].value();}
-
-	void value(size_t id,real v)
-	{return self[id].value(v);}
-
-	void set_bias(size_t id,real v)
-	{self[id].bias=v;}
-};
-
-class network
-{
-	std::vector<layer*> self;
-public:
-	network(std::initializer_list<size_t> form)
-	:
-		self(form.size())
+	void weight(const std::vector<real>& w,const std::vector<real>& b)
 	{
-
-		size_t i=0;
-
-		for(size_t n:form)
-			self[i++]=new layer(n);
-
-		for(size_t i=1;i<self.size();i++)
-			self[i]->connect(self[i-1]);
+		size_t n=0;
+		for(size_t k=0;k<weights.size();k++)//каждый слой
+		for(size_t j=0;j<weights[k].size();j++)//каждый нейрон
+		for(size_t i=0;i<weights[k][j].size();i++)//каждый сосед
+			weights[k][j][i]=w[n++];
+		
+		n=0;
+		for(size_t j=1;j<layers.size();j++)//каждый слой
+		for(size_t i=0;i<layers[j].size();i++)//каждый нейрон
+			layers[j][i].bias=b[n++];
 	}
 
-	std::vector<real> predict(std::initializer_list<real> x)
+	void input(const std::vector<real>& data)
 	{
-		size_t i=0;
-		for(real e:x)
-			self[0]->value(i++,e);
+		assert(data.size()==layers[0].size());
 
-		for(size_t i=1;i<self.size();i++)
-			self[i]->calc();
+		for(size_t i=0;i<data.size();i++)
+			layers[0][i].res=data[i];
+	}
 
-		std::vector<real> r(self.back()->size());
+	std::vector<real> output()
+	{
+		std::vector<real> r(layers.back().size());
 		for(size_t i=0;i<r.size();i++)
-			r[i]=self.back()->value(i);
-
+			r[i]=layers.back()[i].res;
 		return r;
 	}
 
-	void weight(std::vector<real> w)
+	void forward()
 	{
-		size_t n=0;
-
-		for(size_t k=1;k<self.size();k++)
-		for(size_t j=0;j<self[k]->size();j++)
+		for(size_t k=1;k<layers.size();k++)//каждый слой
+		for(size_t j=0;j<layers[k].size();j++)//каждый нейрон
 		{
-			for(size_t i=0;i<self[k-1]->size();i++)
-				self[k]->weight(j,i,w[n++]);
-
-			self[k]->set_bias(j,w[n++]);
+			layers[k][j].res=0;
+			for(size_t i=0;i<layers[k-1].size();i++)//каждый сосед
+				layers[k][j].res+=layers[k-1][i].res*weights[k-1][i][j];
+			layers[k][j].res=f(layers[k][j].res+layers[k][j].bias);
 		}
 	}
 };
 
 int main()
 {
-	network net({2,2,1});
-	net.weight({20,20,-10, -20,-20,30, 20,20,-30 });
+	network test({2,2,1});
+	test.weight({20,20, -20,-20, 20,20},{-10,30,-30});
+	test.input({0,1});
+	test.forward();
 
-	return net.predict({0,1})[0]>.5;
+	return 0;
 }
