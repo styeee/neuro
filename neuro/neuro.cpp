fe3babb84c5d98de37ab071472f78fc9e802e508
Станислав Шубин <sti.99@mail.ru>
2025-11-17 21:42:41 +0700

    good weight and try to create backward

--- neuro/neuro.cpp
+++ neuro/neuro.cpp
@@ -53,6 +53,21 @@ public:
 			layers[j][i].bias=b[n++];
 	}
 
+	void weight(real power)
+	{
+		for(size_t k=0;k<weights.size();k++)//каждый слой
+		for(size_t j=0;j<weights[k].size();j++)//каждый нейрон
+		for(size_t i=0;i<weights[k][j].size();i++)//каждый сосед
+		{
+			const real d=layers[k+1][i].error*layers[k][j].res*power;
+			weights[k][j][i]-=d;
+		}
+
+		for(size_t j=1;j<layers.size();j++)//каждый слой
+		for(size_t i=0;i<layers[j].size();i++)//каждый нейрон
+			layers[j][i].bias-=layers[j][i].error*power;
+	}
+
 	void input(const std::vector<real>& data)
 	{
 		assert(data.size()==layers[0].size());
@@ -80,14 +95,64 @@ public:
 			layers[k][j].res=f(layers[k][j].res+layers[k][j].bias);
 		}
 	}
+
+	void backward(const std::vector<real> target)
+	{
+		for(size_t i=0;i<layers.back().size();i++)
+		{
+			layers.back()[i].error=(target[i]-layers.back()[i].res);
+			layers.back()[i].error*=df(layers.back()[i].res);
+		}
+
+		for(int k=layers.size()-2;k>=0;k--)
+		for(size_t j=0;j<layers[k].size();j++)
+		{
+			layers[k][j].error=0;
+			for(size_t i=0;i<layers[k+1].size();i++)
+			{
+				real const d=layers[k+1][i].error*weights[k][j][i];
+				layers[k][j].error+=d;
+			}
+			layers[k][j].error*=df(layers[k][j].res);
+		}
+	}
+
+	void learn
+	(
+		const std::vector<real>& i,
+		const std::vector<real>& o,
+		size_t count,
+		real power
+	)
+	{
+		input(i);
+		forward();
+		backward(o);
+		weight(power);
+	}
+
+	void good_weight()
+	{
+		weight({20,-20,20,-20,20,20},{-10,30,-30});
+	}
 };
 
 int main()
 {
-	network test({2,2,1});
-	test.weight({20,20, -20,-20, 20,20},{-10,30,-30});
-	test.input({0,1});
-	test.forward();
+    network test({2,2,1});
+	test.good_weight();
+    
+    test.input({0,0}); test.forward(); 
+    std::cout << "0,0 -> " << test.output()[0] << std::endl;
+    
+    test.input({0,1}); test.forward();
+    std::cout << "0,1 -> " << test.output()[0] << std::endl;
+    
+    test.input({1,0}); test.forward();
+    std::cout << "1,0 -> " << test.output()[0] << std::endl;
+    
+    test.input({1,1}); test.forward();
+    std::cout << "1,1 -> " << test.output()[0] << std::endl;
 
-	return 0;
+    return 0;
 }
